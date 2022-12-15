d8304d6154238d3143bf2e0ce12b73795e64be24
ItzNot <dkskd8888@gmail.com>
2022-12-15 12:25:41 +0900

    test

---
+++ test1Project/src/main/java/com/example/test1Project/Test1ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.test1Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test1ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test1ProjectApplication.class, args);
+	}
+
+}
---
+++ test1Project/src/test/java/com/example/test1Project/Test1ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test1Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test1ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
